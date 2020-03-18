028cca2752428739aaf98e18a06e01a461e402d4
termanty <termanty@gmail.com>
2020-03-18 18:31:59 +0200

    Exercises 1.12 and 1.13 ready

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const App = ({ anecdotes}) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0,0,0,0,0,0])
+
+  const onClickNext = () => setSelected(Math.floor(Math.random() * 6))
+  const onClickVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+  
   return (
     <div>
-      <h1>Anekdootit</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={onClickVote}>vote</button>
+      <button onClick={onClickNext}>next anecdote</button>
     </div>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
