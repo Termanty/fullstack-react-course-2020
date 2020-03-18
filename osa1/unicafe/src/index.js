5e00bccfbe56556e676612ee38f01464cffdc986
termanty <termanty@gmail.com>
2020-03-18 16:09:38 +0200

    Exercise 1.6 ready

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ feedback, onClick }) => {
+  return <button onClick={onClick}>{feedback}</button>
+}
+
 const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const onClick = (feedback, setFeedback) => {
+    return () => {
+      setFeedback(feedback + 1)
+    }
+  }
+
   return (
     <div>
-      <h1>Unicafe</h1>
+      <h1>Give Feedback</h1>
+      <Button onClick={onClick(good, setGood)} feedback="good" />
+      <Button onClick={onClick(neutral, setNeutral)} feedback="neutral" />
+      <Button onClick={onClick(bad, setBad)} feedback="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
