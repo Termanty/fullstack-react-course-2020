7fc4b4d91e015362e4fc0d041a26473391990b82
Tero Mäntylä <termanty@gmail.com>
2020-05-04 13:17:14 +0300

    Exercises 4.1, 4.2 ready

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,41 +3,23 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const { MONGODB_URI } = require('./utils/config.js')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
-    console.log('connected to MONGODB') 
+    logger.info('connected to MONGODB') 
   })
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
