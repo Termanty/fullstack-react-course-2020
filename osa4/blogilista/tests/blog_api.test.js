f2c159dab63afecbd9a178fe5cfe02f1734d788e
Tero Mäntylä <termanty@gmail.com>
2020-05-14 16:10:49 +0300

    more supertests

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -8,5 +8,9 @@ test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
-    .expect('Content-Type', )
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
 })
